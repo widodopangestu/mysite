48c9c0c1f1d24056aba3bb2d0d0de6538a303bdb
Widodo Pangestu <wid.pangestu@gmail.com>
2017-02-09 09:15:04 +0700

    lat model

--- books/models.py
+++ books/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
 class Publisher(models.Model):
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=50)
@@ -25,11 +26,51 @@ class Author(models.Model):
     def __str__(self):
         return u'%s %s' % (self.first_name, self.last_name)
 
+class BookManager(models.Manager):
+    def title_count(self, keyword):
+        return self.filter(title__icontains=keyword).count()
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
     publication_date = models.DateField(blank=True, null=True)
+    num_pages = models.IntegerField(blank=True, null=True)
+    objects = BookManager()
 
     def __str__(self):
         return self.title
+
+class MaleManager(models.Manager):
+    def get_queryset(self):
+        return super(MaleManager, self).get_queryset().filter(sex='M')
+
+class FemaleManager(models.Manager):
+    def get_queryset(self):
+        return super(FemaleManager, self).get_queryset().filter(sex='F')
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    sex = models.CharField(max_length=1, choices=(('M', 'Male'),('F', 'Female')))
+    birth_date = models.DateField(blank=True, null=True)
+    people = models.Manager()
+    men = MaleManager()
+    women = FemaleManager()
+
+    def baby_boomer_status(self):
+        import datetime
+        if self.birth_date < datetime.date(1945, 8, 1):
+            return "Pre-boomer"
+        elif self.birth_date < datetime.date(1965, 1, 1):
+            return "Baby boomer"
+        else:
+            return "Post-boomer"
+
+    def _get_full_name(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
+    full_name = property(_get_full_name)
+
+    def __str__(self):
+       return u'%s %s' % (self.first_name, self.last_name)
